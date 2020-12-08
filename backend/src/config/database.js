006cbfdb5aeded1594dccd65d9e5a02d34c22171
Italo Leal <itlcoorporativo@gmail.com>
2020-12-08 11:12:02 -0300

    Adicionando o database da produção

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
-  username: "jonathan.gemaque",
-  password: "jonathan",
-  database: "localiza",
-  host: "localhost",
+  username: "rpvfiltcquscjb",
+  password: "8b19fc10d90ace6d562c992636fa862e9bde6ec63ead84f3368a8c82f63dd613",
+  database: "d2l81ase92jc78",
+  host: "ec2-75-101-212-64.compute-1.amazonaws.com",
   dialect: "postgres",
   define: {
     timestamps: true,
